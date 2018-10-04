f6fb68dea32116520c5886716cb6641a79926b4f
yancloud <lh_ycx@126.com>
2018-10-04 22:18:22 +0800

    finish transport util

--- crawler/transport_util.py
+++ crawler/transport_util.py
@@ -1,37 +1,53 @@
 import paramiko
 import os
+import requests
+import json
+
+from settings import apk_source
 
 
 host = '162.105.175.241'
 port = 22
 username = 'pku'
 password = 'pku2018!))$'
-remote_dir = './Documents/data/'
+remote_dir = './Documents/data/' + apk_source + '/'
 local_dir = './output/'
+remote_apk_dir = './Documents/apk/data/'
+if apk_source == 'Google':
+    remote_apk_dir += 'GooglePlayApk/'
+elif apk_source == 'WDJ':
+    remote_apk_dir += 'WDJApk/' 
 
-
-transport = paramiko.Transport((host,port))
-transport.connect(username=username,password=password)
-sftp = paramiko.SFTPClient.from_transport(transport)
 
 
 # 上传单个文件
 def upload_file(local, remote = ''):
+    transport = paramiko.Transport((host,port))
+    transport.connect(username=username,password=password)
+    sftp = paramiko.SFTPClient.from_transport(transport)
     if remote == '':
         remote = local
     sftp.put(local, remote)
+    sftp.close()
 
 
 # 下载单个文件
 def download_file(remote, local = ''):
+    transport = paramiko.Transport((host,port))
+    transport.connect(username=username,password=password)
+    sftp = paramiko.SFTPClient.from_transport(transport)
     if local == '':
         local = remote
     sftp.get(remote, local)
+    sftp.close()
 
 
 # 上传文件夹
 def upload_dir(local, remote):
     try:
+        transport = paramiko.Transport((host,port))
+        transport.connect(username=username,password=password)
+        sftp = paramiko.SFTPClient.from_transport(transport)
         for root, dirs, files in os.walk(local_dir):
             for filespath in files:
                 local_file = os.path.join(root,filespath)
@@ -53,6 +69,7 @@ def upload_dir(local, remote):
                 except Exception as e:
                     print(e)
         print('upload finish!')
+        sftp.close()
     except Exception as e:
         print(e)
             
@@ -62,6 +79,26 @@ def upload_app(package_name):
     upload_dir(local_dir + package_name, remote_dir)
 
 
+# 下载app
+def download_app():
+    if apk_source == 'Google':
+        response = requests.get('http://162.105.175.241:5000/GooglePlayApk/unfinished')
+        # print(response.content.decode())
+        file_list = json.loads(response.content.decode())[:10]
+        # print(file_list)
+        for f in file_list:
+            try:
+                download_file(remote_apk_dir+f,f)
+                print("download finish with file: " + f)
+                # 在服务器端设置该app为finish状态
+                response = requests.get('http://162.105.175.241:5000/GooglePlayApk/finish/'+f)
+            except Exception as e:
+                print(e)
+        
+        
+
+
 if __name__ == "__main__" :
-    upload_file('spam.log', remote_dir+'spam.log')
-    upload_app('com.douban.movie')
+    #upload_file('spam.log', remote_dir+'spam.log')
+    #upload_app('com.douban.movie')
+    download_app()
